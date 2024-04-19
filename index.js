6bb56c10a29ca63be42b43ce955553588569414c
JackieRandle <159210475+JackieRandle@users.noreply.github.com>
2024-04-18 20:02:22 -0500

    completed

---
+++ index.js
@@ -0,0 +1,40 @@
+// document.getElementById("count").innerText = 5
+let count = 5
+//count = count + 1
+console.log(count)
+
+
+function increment() {
+    console.log("The button was clicked")
+
+}
+
+function countdown() {
+    console.log(42)
+
+    Rundown()   
+    //Let's Start!
+    // Move Faster!
+    //Stop!
+}
+
+    let count = 1
+    //count = count + 1
+
+function lapsCompleted(
+
+    console.log(lapsCompleted)
+)
+
+
+    console.log("clicked")
+
+}
+let count = 0
+function increment() {
+//count = count + 1
+console.log(count)
+
+}
+
+
