9030798580ba8a6de687de8f698e7a961b041ef2
JackieRandle <159210475+JackieRandle@users.noreply.github.com>
2024-04-22 20:31:37 -0500

    people-counter

--- index.js
+++ index.js
@@ -1,40 +1,42 @@
 // document.getElementById("count").innerText = 5
-let count = 5
-//count = count + 1
-console.log(count)
+// let count = document.getElementById("count-el")
+// let count = 5
+
+// }function save() {
+//     count = count + 1
+//     countEL.innerText = count
+//     console.log(count)
+// }
+// let username = "per" 
+
+// console.log(username)
 
 
-function increment() {
-    console.log("The button was clicked")
+// let message = "You have three new notificiations"
 
-}
+// let messageToUser = message  + "," + username + "!"
 
-function countdown() {
-    console.log(42)
+// console.log(message)
 
-    Rundown()   
-    //Let's Start!
-    // Move Faster!
-    //Stop!
-}
+// let name = "Jacqueline"
+// let greeting = "Hi, my name is "
+// let myGreeting = " greeting + name"
 
-    let count = 1
-    //count = count + 1
+// console.log(myGreeting)
 
-function lapsCompleted(
+// let points = 4
+// letPoints = "10"
 
-    console.log(lapsCompleted)
-)
+// console.log(4 + 5)= 9
+// console.log("2" + "4") = 24
+// console.log("5" + 1) = 51
+// console.log(100 + "100") = 100100
 
+let welcomeEL = document.getElementById("welcome-el")
+let myName = "Jacqueline"
+let greeting = "Hi, my name is "
+let myGreeting =  greeting + myName 
 
-    console.log("clicked")
-
-}
-let count = 0
-function increment() {
-//count = count + 1
-console.log(count)
-
-}
+welcomeEL.innerText = myGreeting
 
 
